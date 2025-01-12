6cafee361c30be2febeb1bbd5f8a1aa76f784c4a
DeAssisMoreira <vladospozhar@yandex.ru>
2025-01-12 16:31:43 +0300

    HW2

---
+++ Script.js
@@ -0,0 +1,49 @@
+let a = 10;
+alert(10);
+
+let b = 2007;
+alert(2007);
+
+let c = 'Brendan Eich';
+alert('Brendan Eich');
+
+let d = 10;
+let e = 2;
+let sum = d + e;
+let difference = d - e;
+let product = d * e;
+let quotient = d / e;
+let power = e ** 5;
+alert(sum);
+alert(difference);
+alert(product);
+alert(quotient);
+alert(power);
+
+let f = 9;
+let remainder = f % e;
+alert(remainder);
+
+let g = 1;
+g = g + 5;
+g = g - 3;
+g = g * 7;
+g = g / 3;
+g = g + 1;
+g = g - 1;
+alert(g);
+
+let age = prompt('Сколько вам лет?');
+alert(age);
+
+const user = {
+name: 'Vlad',
+age: 30,
+'isAdmin': true
+};
+console.log(user['isAdmin']);
+alert(user);
+
+let i = prompt('Как тебя зовут?');
+console.log('Привет, ${i}');
+alert('Привет,'), (i);
