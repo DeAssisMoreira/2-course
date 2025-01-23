6448a8825975970a2417a388261d089aceb8e3fe
DeAssisMoreira <vladospozhar@yandex.ru>
2025-01-23 03:58:35 +0300

    HW_3

--- Script.js
+++ Script.js
@@ -1,4 +1,94 @@
-let a = 10;
+
+/* Задание 1
+
+let password = 'qwerty';
+let pass = prompt('Введите пароль');
+alert(pass = password ? "Верно" : "Ошибка"); */
+
+
+
+let password = 'pass';
+let pass = prompt('Введите пароль');
+if (pass === password) {
+alert('Верно');
+} else {
+alert('Ошибка');
+}
+
+/* Задание 2 */
+
+let c = 4
+if (c > 0 && c < 10) {
+    alert('Верно');
+} else {
+    alert('Неверно');
+}
+
+/* Задание 3 */
+
+let d = 78;
+let e = 178;
+if (e >= 100 || d >=100) {
+    alert('Верно');
+} else {
+    alert('Неверно');
+}
+    
+/* Задание 4 */
+
+let a = '2';
+let b = '3';
+alert(Number(a) + Number(b));
+
+/* Задание 5 */
+
+let monthNumber = 12;
+switch (monthNumber) {
+    case 1:
+        alert('Зима');
+        break;
+    case 2:
+        alert('Зима');
+        break;
+    case 3:
+        alert('Весна');
+        break;
+    case 4:
+        alert('Весна');
+        break;
+    case 5:
+        alert('Весна');
+        break;
+    case 6:
+        alert('Лето');
+        break;
+    case 7:
+        alert('Лето');
+        break;
+    case 8:
+        alert('Лето');
+        break;
+    case 9:
+        alert('Осень');
+        break;
+    case 10:
+        alert('Осень');
+        break;
+    case 11:
+        alert('Осень');
+        break;
+    case 12:
+        alert('Зима');
+        break;
+    default:
+        alert('Неверно');
+        break;
+} 
+
+
+
+
+/*let a = 10;
 alert(10);
 a = 20;
 alert(a);
@@ -48,4 +138,5 @@ alert(user);
 let userName = prompt('Как тебя зовут?');
 console.log(`Привет, ${userName}`);
 alert(`Привет, ${userName}!`);
-<script src="./Script.js"></script>
+<script src="./Script.js"></script> */
+
